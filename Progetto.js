947602b97b7703874b72664424951fef6d323f88
Justy116 <giustinogragnaniello16@gmail.com>
2021-07-25 10:18:59 +0200

    Primo progetto js-basic

---
+++ Progetto.js
@@ -0,0 +1,12 @@
+var contatore = 0;
+
+function incrementaNumero(){
+   contatore++;
+   document.getElementById("numero").innerHTML = contatore;
+ }
+
+ function diminuisciNumero(){
+          contatore--;
+    document.getElementById("numero").innerHTML = contatore;
+ }
+ 
